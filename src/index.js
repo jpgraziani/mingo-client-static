d7091f50ebad6d7215f7310ff33f6c499327dd58
Jonathan Graziani <jpgraziani@gmail.com>
2021-03-03 14:12:55 -0700

    scroll to top page feature

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import ScrollToTop from './components/ScrollToTop'
 import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root')
 );
